860be230fd485f8b912ac983f7d6d9651e8aec46
Amit Saha <asaha.bwn2000@gmail.com>
2023-10-25 22:14:47 +0530

    can't think of any commit message

--- script.js
+++ script.js
@@ -27,22 +27,43 @@ const videoContainerAnimation = () => {
       });
     });
   };
-  videoContainerAnimation();
+videoContainerAnimation();
 
-  const loadingAnimation = () => {
-    gsap.from("#page1 h1", {
-      y: 90,
-      opacity: 0,
-      delay: 0.5,
-      duration: 0.9,
-      stagger: 0.3,
-    });
-    gsap.from("#page1 #video-container", {
-      scale: 0.9,
-      opacity: 0,
-      delay: 1.3,
-      duration: 0.3,
-    });
-  };
-  loadingAnimation();
+const loadingAnimation = () => {
+  gsap.from("#page1 h1", {
+    y: 90,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.3,
+  });
+  gsap.from("#page1 #video-container", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.3,
+  });
+};
+loadingAnimation();
   
+
+document.addEventListener("mousemove", (details) => {
+  gsap.to("#cursor", {
+    left: details.x,
+    top: details.y,
+  });
+});
+
+const product = document.querySelectorAll(".child");
+product.forEach(function (items) {
+  items.addEventListener("mouseenter", () => {
+    gsap.to("#cursor", {
+      transform: `translate(-50%, -50%) scale(1)`,
+    });
+  });
+  items.addEventListener("mouseleave", () => {
+    gsap.to("#cursor", {
+      transform: `translate(-50%, -50%) scale(0)`,
+    });
+  });
+});
